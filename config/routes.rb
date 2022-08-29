d11040d829f1530c7b34ee702ad885735637fa5c
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-30 02:00:55 +0300

    update: route with recipe inside user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
+  resources :recipes
   end
   # Defines the root path route ("/")
   # root "articles#index"
