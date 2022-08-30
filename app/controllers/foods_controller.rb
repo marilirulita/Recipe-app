41ee05a1a9276007cc1cbf4b382f1e230af1ba1d
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-30 19:14:45 +0300

    Fix: linter issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,18 +1,16 @@
 class FoodsController < ApplicationController
-  # todo: require user to login for all the actions
+  # TODO: require user to login for all the actions
   def index
-    @my_foods=Food.where(user: current_user)
+    @my_foods = Food.where(user: current_user)
   end
 
   def show
-    puts "showing a spacific food"
+    puts 'showing a spacific food'
   end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
   def destroy
     Food.find(params[:id]).destroy
