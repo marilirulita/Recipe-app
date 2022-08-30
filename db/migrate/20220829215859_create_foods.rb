7dd6b5e6723be8c681f30ec75e2c0a1ee93cf1a7
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-30 17:02:09 +0300

    Create: foods model and migration

--- db/migrate/20220829215859_create_foods.rb
+++ db/migrate/20220829215859_create_foods.rb
@@ -1,7 +1,12 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false, unique: true
+      t.integer :preparation_time, null: false
+      t.integer :cooking_time, null: false
+      t.text :description
+      t.boolean :public, null: false, default: true
       t.timestamps
     end
   end
