7411237298b9b3844e02982e60e0fb8283fd4b4c
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-30 21:18:39 +0300

    Fix: linter issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,17 +9,18 @@ class FoodsController < ApplicationController
   end
 
   def new
-    @new_food=Food.new
+    @new_food = Food.new
   end
 
   def create
-    # puts params[:food]
-    @new_food=Food.new(user: current_user,name: params[:name], price: params[:price],measurement_unit: params[:measurement_unit])
+    @new_food = Food.new(user: current_user, name: params[:name], price: params[:price],
+                         measurement_unit: params[:measurement_unit])
     @new_food.save!
     redirect_to user_foods_path
   end
 
   def destroy
     Food.find(params[:id]).destroy
+    redirect_to user_foods_path
   end
 end
