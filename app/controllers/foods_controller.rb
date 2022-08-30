ff0aff9f93f430dbb6ecbd670678c577c48bb113
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-30 21:14:58 +0300

    Implemnt: add new food form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,9 +8,16 @@ class FoodsController < ApplicationController
     puts 'showing a spacific food'
   end
 
-  def new; end
+  def new
+    @new_food=Food.new
+  end
 
-  def create; end
+  def create
+    # puts params[:food]
+    @new_food=Food.new(user: current_user,name: params[:name], price: params[:price],measurement_unit: params[:measurement_unit])
+    @new_food.save!
+    redirect_to user_foods_path
+  end
 
   def destroy
     Food.find(params[:id]).destroy
