21c2cadd9bd2a9a103c8e9d0db8c717d2eefc6c1
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-31 23:27:46 +0300

    Write query: the list of food that is missing for all recipes of the logged-in user

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -1,5 +1,9 @@
 class ShopingListsController < ApplicationController
   def index
-    @shoping_list=[]
+    #the list of food that is missing for all recipes of the logged-in user
+    @shoping_lists=Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
+    .where(user: current_user)
+    .joins(:recipe_foods, :recipes)
+    .where.not(:recipes=>{:user => current_user}).group(:name)
   end
 end
