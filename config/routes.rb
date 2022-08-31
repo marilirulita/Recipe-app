96208a0e78b135bbedd6ef5a032e245b929c035b
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-31 20:29:11 +0300

    Remove: shoping list route before user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'shoping_lists/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
