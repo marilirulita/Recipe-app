407d143067d7b68cba977fbe54acf06ccf3eb92d
mar <marilirulita@gmail.com>
2022-08-31 02:14:52 -0700

    add style to fis style linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,8 +7,7 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show
-  end
+  def show; end
 
   # GET /recipes/new
   def new
@@ -30,7 +29,6 @@ class RecipesController < ApplicationController
       redirect_to new_recipe_path
     end
   end
-
 
   # DELETE /recipes/1 or /recipes/1.json
   def destroy
