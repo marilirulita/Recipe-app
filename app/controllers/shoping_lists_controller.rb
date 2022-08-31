085ac39b5e6a9005008dc1ee271df4ad2db52eaf
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-31 23:37:55 +0300

    Fix: linter issue

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -1,9 +1,9 @@
 class ShopingListsController < ApplicationController
   def index
-    #the list of food that is missing for all recipes of the logged-in user
-    @shoping_lists=Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
-    .where(user: current_user)
-    .joins(:recipe_foods, :recipes)
-    .where.not(:recipes=>{:user => current_user}).group(:name)
+    # the list of food that is missing for all recipes of the logged-in user
+    @shoping_lists = Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
+      .where(user: current_user)
+      .joins(:recipe_foods, :recipes)
+      .where.not(recipes: { user: current_user }).group(:name)
   end
 end
