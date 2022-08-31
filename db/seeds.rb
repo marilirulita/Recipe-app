4c925b7c52a33d4d82e8e588ebddccda32c8eca1
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-31 20:20:13 +0300

    Update: seed based to add recipe_foods

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,27 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-userOne=User.create(name: 'Tadesse Alemayehu',email: "tadesse@gmail.com", password: '123456')
-userTwo=User.create(name: 'mar y sol Bautista',email: "mar@gmail.com", password: '123456')
+@user_one=User.create(name: 'Tadesse Alemayehu',email: "tadesse@gmail.com", password: '123456')
+@user_two=User.create(name: 'mar y sol Bautista',email: "mar@gmail.com", password: '123456')
 
 (1..5).each do |i|
-  Food.create(user: userOne,name: "tibs#{i}",price: 120+i, quantity: i)
+  Food.create(user: @user_one,name: "tibs#{i}",price: 120+i, quantity: i)
 end
 (1..5).each do |i|
-  Recipe.create(user: userTwo,name: "tibs#{i}",preparation_time: "#{10+i}", cooking_time: "#{12+i}", public: true)
+  Food.create(user: @user_two,name: "kekel#{i}",price: 120+i, quantity: i)
+end
+(1..5).each do |i|
+  Recipe.create(user: @user_one,name: "recipe#{i}",preparation_time: "#{10+i}", cooking_time: "#{12+i}", public: true)
+end
+(1..5).each do |i|
+  Recipe.create(user: @user_two,name: "recipe#{i}",preparation_time: "#{10+i}", cooking_time: "#{12+i}", public: true)
+end
+
+#foods_recipes
+
+@recipes=Recipe.all
+Food.all.each do |food|
+  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1])
+  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1])
+  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1])
 end
