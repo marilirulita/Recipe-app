5c0cdb3de93ebd29c23c87a5e6cd266e58ae7ee5
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-01 20:06:12 +0300

    Test: login functions

--- spec/requests/shoping_lists_spec.rb
+++ spec/requests/shoping_lists_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe 'ShopingLists', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/shoping_lists/index'
-      expect(response).to have_http_status(:success)
+      puts user_shoping_lists_path(user_id: 1)
+      get user_shoping_lists_path(user_id: 1)
+      ## TODO: testing for redirection sense no user signed in
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe 'Users', type: :system do
       expect(page).to have_content 'Log in'
     end
   end
+  describe "test description" do
+      # before(:all){}
+      it "test login" do
+        @user_one=User.create(name: 'Tadesse Alemayehu',email: "testTadesse@gmail.com", password: '123456')
+        visit new_user_session_path
+        within('#new_user') do
+          fill_in 'user[email]', with: 'testTadesse@gmail.com'
+          fill_in 'user[password]', with: '123456'
+        end
+        click_button('commit')
+        expect(page).to have_content('Signed in successfully.')
+      end
+    end
 end
