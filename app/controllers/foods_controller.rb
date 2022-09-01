ece37b8b0695912eebb4c6bbb5d0c3673cd05a38
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-01 08:57:12 +0300

    Remove: unnecessary redirect from user index

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,6 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
   def index
-    redirect_to new_user_session_path unless user_signed_in?
     @my_foods = Food.where(user: current_user)
   end
 
