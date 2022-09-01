6dcd4a0e14e5703e887d4e4ae9942796a45a6624
mar <marilirulita@gmail.com>
2022-09-01 01:26:17 -0700

    sdd midding data devise cnfig

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :food
-  has_many :recipes
+  has_many :food, dependent: :delete_all
+  has_many :recipes, dependent: :delete_all
+
+  validates :name, presence: true
 end
