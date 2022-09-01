4a81686ed1268ee29c6b0f98d1252d0385243218
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-09-01 15:38:58 +0300

    Fix: rubocop linter issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -17,7 +17,7 @@ class FoodsController < ApplicationController
   end
 
   def destroy
-    food=Food.find(params[:id])
+    food = Food.find(params[:id])
     RecipeFood.where(food: food).destroy_all
     food.destroy
     redirect_to user_foods_path
