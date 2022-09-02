12bb8f9605d5b5ae1cb083792ac51a5495d96d53
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-02 11:08:10 +0300

    Update: shoping_list view

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -1,10 +1,14 @@
 class ShopingListsController < ApplicationController
   before_action :authenticate_user!
+  layout 'user'
   def index
     # the list of food that is missing for all recipes of the logged-in user
     @shoping_lists = Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
       .where(user: current_user)
       .joins(:recipe_foods, :recipes).select('count(recipes.name) as recipes')
       .where.not(recipes: { user: current_user }).group(:name)
+
+      @total_value=@shoping_lists.reduce(0) {|sum,list| sum+list.price}
+      @total_amount=@shoping_lists.length
   end
 end
