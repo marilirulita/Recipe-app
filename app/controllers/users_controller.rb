bbf7c13b0128b5fe98ce8b60171cd14a59b4809f
mar <marilirulita@gmail.com>
2022-09-02 16:54:56 -0700

    delete unecesary files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  layout 'user'
   def index
-    redirect_to user_path(id: current_user)
   end
-
-  def show; end
 end
