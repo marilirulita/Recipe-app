2157f5846e9ecfb0145985d54d5f3792bca91a73
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-02 11:49:52 +0300

    Add: sorting functions to Shoping list

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -6,9 +6,22 @@ class ShopingListsController < ApplicationController
     @shoping_lists = Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
       .where(user: current_user)
       .joins(:recipe_foods, :recipes).select('count(recipes.name) as recipes')
-      .where.not(recipes: { user: current_user }).group(:name)
-
+      .where.not(recipes: { user: current_user })
+      .group(:name)
       @total_value=@shoping_lists.reduce(0) {|sum,list| sum+list.price}
       @total_amount=@shoping_lists.length
+      @shoping_lists=sort_helper(row: 'quantity',order: 'asc')
   end
 end
+  def sort_helper(row: 'price', order: 'asc')
+    case row
+    when 'price'
+      return @shoping_lists.sort_by{|store| store.price} unless order!='asc'
+      @shoping_lists.sort_by{|store| store.price}
+      return @shoping_lists.reverse
+    else
+      return @shoping_lists.sort_by{|store| store.quantity} unless order!='asc'
+      @shoping_lists.sort_by{|store| store.quantity}
+      return @shoping_lists.reverse
+    end
+  end
