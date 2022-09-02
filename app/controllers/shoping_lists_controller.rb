e05e4ece003fb5e4effe4fef1ef97afbb7e4b8fc
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-09-02 15:08:03 +0300

    Add: unit to the shoping list

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -3,11 +3,12 @@ class ShopingListsController < ApplicationController
   layout 'user'
   def index
     # the list of food that is missing for all recipes of the logged-in user
-    @shoping_lists = Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
+    @shoping_lists = Food.select('foods.name, foods.measurement_unit,
+      sum(foods.quantity) as quantity, sum(foods.price) as price')
       .where(user: current_user)
       .joins(:recipe_foods, :recipes).select('count(recipes.name) as recipes')
       .where.not(recipes: { user: current_user })
-      .group(:name)
+      .group(:name, :measurement_unit)
     @total_value = @shoping_lists.reduce(0) { |sum, list| sum + list.price }
     @total_amount = @shoping_lists.length
     @shoping_lists = sort_helper(params[:row], params[:order]) if params[:sort]
