dfc3897042ed3f2de2d4503b11953d168401ec5c
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-02 09:21:24 +0300

    update: General shopping list view as the requirement

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -4,7 +4,7 @@ class ShopingListsController < ApplicationController
     # the list of food that is missing for all recipes of the logged-in user
     @shoping_lists = Food.select('foods.name, sum(foods.quantity) as quantity, sum(foods.price) as price')
       .where(user: current_user)
-      .joins(:recipe_foods, :recipes)
+      .joins(:recipe_foods, :recipes).select("count(recipes.name) as recipes")
       .where.not(recipes: { user: current_user }).group(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@
 @user_one=User.create(name: 'Tadesse Alemayehu',email: "tadesse@gmail.com", password: '123456')
 @user_two=User.create(name: 'mar y sol Bautista',email: "mar@gmail.com", password: '123456')
 
+#once user are created
+@user_one=User.first
+@user_two=User.last
+
 (1..5).each do |i|
   Food.create(user: @user_one,name: "tibs#{i}",price: 120+i, quantity: i)
 end
@@ -16,17 +20,24 @@ end
   Food.create(user: @user_two,name: "kekel#{i}",price: 120+i, quantity: i)
 end
 (1..5).each do |i|
-  Recipe.create(user: @user_one,name: "recipe#{i}",preparation_time: "#{10+i}", cooking_time: "#{12+i}", public: true)
+  Recipe.create(user: @user_one,
+    name: "recipe#{i}",description: "description text" ,
+    preparation_time: "#{10+i}",
+    cooking_time: "#{12+i}", public: true)
 end
+
 (1..5).each do |i|
-  Recipe.create(user: @user_two,name: "recipe#{i}",preparation_time: "#{10+i}", cooking_time: "#{12+i}", public: true)
+  Recipe.create(user: @user_two,
+    name: "recipe#{i}",description: "description text" ,
+    preparation_time: "#{10+i}",
+    cooking_time: "#{12+i}", public: true)
 end
 
 #foods_recipes
 
 @recipes=Recipe.all
 Food.all.each do |food|
-  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1])
-  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1])
-  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1])
+  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1], quantity: 2)
+  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1], quantity: 2)
+  RecipeFood.create!(food: food, recipe: @recipes[rand 0..@recipes.length-1], quantity: 2)
 end
