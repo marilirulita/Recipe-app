8586ba05271b1ad12ddd03b538cbfc6c6773284a
mar <marilirulita@gmail.com>
2022-09-02 21:39:55 -0700

    add fods new form missing quantity input

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,15 +11,16 @@ class FoodsController < ApplicationController
 
   def create
     @new_food = Food.new(user: current_user, name: params[:name], price: params[:price],
-                         measurement_unit: params[:measurement_unit])
+    measurement_unit: params[:measurement_unit], quantity: params[:quantity])
     @new_food.save!
-    redirect_to user_foods_path
+    redirect_to foods_path
   end
 
   def destroy
     destroy_food = Food.find(params[:id])
     RecipeFood.where(food: destroy_food).destroy_all
     destroy_food.destroy
-    redirect_to user_foods_path
+    redirect_to foods_path
   end
+
 end
