b86364c41ac38e4b93cbcfda32336105cdca51ae
mar <marilirulita@gmail.com>
2022-09-02 20:37:57 -0700

    add style in food index vies

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -1,6 +1,5 @@
 class ShopingListsController < ApplicationController
   before_action :authenticate_user!
-  layout 'user'
   def index
     # the list of food that is missing for all recipes of the logged-in user
     @shoping_lists = Food.select('foods.name, foods.measurement_unit,
